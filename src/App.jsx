4b60b4b237c2bd51c62fb81aac7b75cf91cb5aff
Elvin James <elvinjames2528@gmail.com>
2025-01-29 16:06:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ export default function App() {
     return (
       <div className="text-center">
         <img
-          src="https://unsplash.com/photos/a-cup-of-coffee-sitting-on-top-of-a-bed-next-to-a-window-M43h7IwSPcI"
+          src="catwalk.gif"
           alt="Cat walking"
           className="mx-auto h-50 w-50 cursor-pointer"
           onClick={handleClick}
